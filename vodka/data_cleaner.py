a3647ab06e9410537a8947f39a1e38b79f769747
Tung Hoang <sontunghoang@gmail.com>
2020-05-16 21:28:39 +0200

    Add docs for vodka

--- vodka/data_cleaner.py
+++ vodka/data_cleaner.py
@@ -4,21 +4,50 @@ import datetime
 
 
 def clean_country(country):
+    """Clean country data
+
+    Args:
+        country (str): 
+    Returns:
+        country starts with capital letter
+    """
     # start with capial letter
     return country.title()
 
 
 def clean_firstname(name):
+    """Clean firstname
+
+    Args:
+        name (str): 
+    Returns:
+        firstname starts with capital letter
+    """
     # start with capial letter
     return name.title()
 
 
 def clean_lastname(name):
+    """Clean lastname
+
+    Args:
+        name (str): 
+    Returns:
+        lastname starts with capital letter
+    """
     # start with capial letter
     return name.title()
 
 
 def clean_datetime(dt):
+    """Change datetime format from European
+    m/d/Y to US Y-m-d format
+
+    Args:
+        dt (str): datetime string in m/d/Y format
+    Returns:
+        datetime string in Y-m-d format
+    """
     try:
         north_amercia_dt = datetime.datetime.strptime(
             dt, '%m/%d/%Y').strftime('%Y-%m-%d')
@@ -28,15 +57,28 @@ def clean_datetime(dt):
 
 
 def _dict_clean(d):
+    """ Replace None with empty string in dict
+    Args:
+        d (dict): dictionary
+    Returns:
+        dict
+    """
     result = {}
     for key, value in d.items():
         if value is None:
-            value = 'default'
+            value = ''
         result[key] = value
     return result
 
 
 def perform_full_clean(data):
+    """Perform a full clean on all fields of the data
+
+    Args:
+        data (dict): 
+    Returns:
+        cleaned data
+    """
     cleaned_data = copy.deepcopy(data)
     cleaned_data = _dict_clean(cleaned_data)
     # Country
@@ -45,7 +87,8 @@ def perform_full_clean(data):
     cleaned_data["first_name"] = clean_firstname(
         cleaned_data.get("first_name", ""))
     # Lastname
-    cleaned_data["last_name"] = clean_lastname(cleaned_data.get("last_name", ""))
+    cleaned_data["last_name"] = clean_lastname(
+        cleaned_data.get("last_name", ""))
     # date
     cleaned_data["date"] = clean_datetime(cleaned_data.get("date", ""))
     return cleaned_data
--- vodka/vodka.py
+++ vodka/vodka.py
@@ -1,4 +1,15 @@
-"""Example Kafka consumer."""
+# -*- coding: utf-8 -*-
+"""Vodka
+Vodka is the a Faust (https://github.com/robinhood/faust) worker
+
+It contains 3 Faust agents
+
+1. Clean the raw message
+2. Sink the cleaned message to a new Kafka topic
+3. Subscribe to the topic contained cleaned data and calculate
+some data statistics over a window of time.
+
+"""
 import os
 import faust
 from datetime import timedelta
@@ -14,9 +25,14 @@ app = faust.App('vodka',
                 value_serializer='json',
                 consumer_auto_offset_reset='latest')
 
+# raw data topic
 user_topic = app.topic(USER_TOPIC)
+# topic contains cleaned data
 cleaned_user_topic = app.topic(CLEANED_USER_TOPIC, internal=True, partitions=1)
 
+# Faust windowed table, serve as storage for
+# statistics calculation
+# refer to https://faust.readthedocs.io/en/latest/userguide/tables.html
 counts = app.Table('user_activity_count',
                    partitions=1,
                    key_type=str,
@@ -25,12 +41,14 @@ counts = app.Table('user_activity_count',
                                          key_index=False)
 
 
+# Faust agent to publish the cleaned message
 @app.agent(sink=[cleaned_user_topic])
 async def publish_cleaned_message(stream):
     async for message in stream:
         yield message
 
 
+# Faust agent to perform data cleaning
 @app.agent(user_topic, sink=[publish_cleaned_message])
 async def clean_data(data):
     async for d in data:
@@ -38,6 +56,7 @@ async def clean_data(data):
         yield cleaned_data
 
 
+# Faust agent to perform data stats calculation
 @app.agent(cleaned_user_topic)
 async def log_derived_data(data):
     async for d in data:
@@ -51,8 +70,6 @@ async def log_derived_data(data):
         email = d["email"]
         counts[email] += 1
         if counts[email].value() >= 5:
-            print(
-                f'{email} has now appeared multiple times'
-            )
+            print(f'{email} has now appeared multiple times')
         # log derived data
         print("derived data: ", d)
