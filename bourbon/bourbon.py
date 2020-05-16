d3992621e6874a4c936f82b14d0932ed5507981f
Tung Hoang <sontunghoang@gmail.com>
2020-05-16 18:40:15 +0200

    Add gatekeeper bourbon

--- bourbon/bourbon.py
+++ bourbon/bourbon.py
@@ -3,16 +3,18 @@ import os
 import faust
 from datetime import timedelta
 
-KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL', 'broker:9092')
+import data_quality_controller as controller
 
-CLEANED_USER_TOPIC = os.environ.get('CLEANED_USER_TOPIC')
+KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL', 'broker:9092')
+USER_TOPIC = os.environ.get('USER_TOPIC')
+DATAQUALITY_TOPIC = os.environ.get('DATAQUALITY_TOPIC')
 
 app = faust.App('bourbon',
                 broker=KAFKA_BROKER_URL,
-                value_serializer='json',
-                consumer_auto_offset_reset='latest')
+                value_serializer='json')
 
-cleaned_user_topic = app.topic(CLEANED_USER_TOPIC, internal=True, partitions=1)
+user_topic = app.topic(USER_TOPIC)
+quality_topic = app.topic(DATAQUALITY_TOPIC, internal=True, partitions=1)
 
 counts = app.Table('user_activity_count',
                    partitions=1,
@@ -22,19 +24,21 @@ counts = app.Table('user_activity_count',
                                          key_index=False)
 
 
-@app.agent(cleaned_user_topic)
-async def log_derived_data(data):
+@app.agent(sink=[quality_topic])
+async def publish_quality_message(stream):
+    async for message in stream:
+        yield message
+
+
+@app.agent(user_topic, sink=[publish_quality_message])
+async def perform_quality_check(data):
     async for d in data:
-        # count country
-        country = d["country"]
-        counts[country] += 1
+        # perform check
+        check_res = controller.perform_full_check(d)
+        yield check_res
 
-        print(f'{country} has now appeared {counts[country].value()} times')
 
-        # count email address
-        email = d["email"]
-        counts[email] += 1
-        if counts[email].value() >= 5:
-            print(f'{email} has now appeared multiple times')
-        # log derived data
-        print("derived data: ", d)
+@app.agent(quality_topic)
+async def gate_keeper(data):
+    async for d in data:
+        print(d)
