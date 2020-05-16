0e59967855f1c5e453fa0f09de2f806bf61cf5fc
Tung Hoang <sontunghoang@gmail.com>
2020-05-16 17:39:54 +0200

    Use windows function to count

--- vodka/vodka.py
+++ vodka/vodka.py
@@ -1,6 +1,7 @@
 """Example Kafka consumer."""
 import os
 import faust
+from datetime import timedelta
 
 from data_cleaner import perform_full_clean
 
@@ -14,7 +15,14 @@ app = faust.App('vodka',
                 consumer_auto_offset_reset='latest')
 
 user_topic = app.topic(USER_TOPIC)
-cleaned_user_topic = app.topic(CLEANED_USER_TOPIC)
+cleaned_user_topic = app.topic(CLEANED_USER_TOPIC, internal=True, partitions=1)
+
+counts = app.Table('user_activity_count',
+                   partitions=1,
+                   key_type=str,
+                   default=int).tumbling(timedelta(minutes=1),
+                                         expires=timedelta(minutes=5),
+                                         key_index=False)
 
 
 @app.agent(sink=[cleaned_user_topic])
@@ -33,4 +41,9 @@ async def clean_data(data):
 @app.agent(cleaned_user_topic)
 async def log_derived_data(data):
     async for d in data:
+        # count country
+        country = d["country"]
+        counts[country] += 1
+        print(f'{country} has now appeared {counts[country].value()} times')
+        # log derived data
         print("derived data: ", d)
